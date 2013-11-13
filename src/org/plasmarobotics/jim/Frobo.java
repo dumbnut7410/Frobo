f7826936f9d41ba64596bb71173714b7e5c5d116
jim <jim@swag>
2013-11-12 17:37:07 -0700

    first commit on laptop

--- src/org/plasmarobotics/jim/Frobo.java
+++ src/org/plasmarobotics/jim/Frobo.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-
+//laptop edition
 
 package org.plasmarobotics.jim;
 
