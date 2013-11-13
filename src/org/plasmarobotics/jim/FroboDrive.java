7d022fe0da989851ff943c1004603af74628388d
jim <jim@swag>
2013-11-12 17:24:45 -0700

    first commit on laptop

--- src/org/plasmarobotics/jim/FroboDrive.java
+++ src/org/plasmarobotics/jim/FroboDrive.java
@@ -6,10 +6,10 @@ package org.plasmarobotics.jim;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Talon;
+
 
 /**
- *This class controls the drive system of Frobo
+ *This class controls the drive system 
  * @author Jim
  */
 
@@ -32,7 +32,10 @@ public class FroboDrive {
         this.rightJoystick = rightJoystick;
         
         //Creates a RobotDrive...
-        chassis = new RobotDrive(Constants.FRONT_LEFT_DRIVE_PORT, Constants.BACK_LEFT_DRIVE_PORT, Constants.FRONT_RIGHT_DRIVE_PORT, Constants.BACK_RIGHT_DRIVE_PORT);
+        chassis = new RobotDrive(Constants.FRONT_LEFT_DRIVE_PORT, 
+                Constants.BACK_LEFT_DRIVE_PORT, 
+                Constants.FRONT_RIGHT_DRIVE_PORT, 
+                Constants.BACK_RIGHT_DRIVE_PORT);
         
     }
     
