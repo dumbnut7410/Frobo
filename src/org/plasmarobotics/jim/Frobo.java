65208311e8bb570e9418cf6d4cfb3cbc31eb37ba
Jim <jchainsaw@hotmail.com>
2013-11-12 17:33:25 -0700

    initial desktop commit

--- src/org/plasmarobotics/jim/Frobo.java
+++ src/org/plasmarobotics/jim/Frobo.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+//desktop version
 //monsters consumed: 1
 
 package org.plasmarobotics.jim;
